ab0c63a197713e1b9972c3c984faa9c0c41e47ea
OkochaEH <oussamaayachefff@gmail.com>
2022-05-22 19:03:41 +0100

    add the change background features

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-let panels = document.querySelectorAll(".panel"); 
-panels.forEach( panel => {
+let panels = document.querySelectorAll(".panel");
+let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+let hexColor = [];
+
+panels.forEach((panel) => {
   panel.addEventListener("click", () => {
-    removeActiveClass()
+    removeActiveClass();
     panel.classList.add("active");
-  })
-})
-function removeActiveClass()  {
-  panels.forEach( panel => {
+    changeBackground();
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach((panel) => {
     panel.classList.remove("active");
-  })
+  });
+}
+
+function changeBackground() {
+  for (let i = 0; i < 6; i++) {
+    const randomNum = Math.floor(Math.random() * hex.length);
+    const color = hex[randomNum];
+    hexColor.push(color);
+  }
+  const finalColor = `#${hexColor.join("")}`;
+  document.body.style.background = finalColor;
+  hexColor = [];
 }
